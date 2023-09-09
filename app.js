a39e19964401e7365ac1b6609a157376fe73657e
Bijay Bikram <becauseiam33@gmail.com>
2023-09-09 22:18:22 +0545

    create, read and delete operations done

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // allBlog
-app.get("/", (req, res) => {
-  res.render("blogs");
+app.get("/", async (req, res) => {
+  //table bata data nikalna paryo
+  const allBlogs = await blogs.findAll();
+  // console.log(allBlogs);
+  res.render("blogs", { everyBlogs: allBlogs });
 });
 
 // createblog
@@ -22,7 +25,33 @@ app.get("/createblog", (req, res) => {
   res.render("createBlog");
 });
 
+// single blog page
+app.get("/single/:id", async (req, res) => {
+  const id = req.params.id;
+
+  //id ko data magna paryo
+  const blog = await blogs.findAll({ where: { id: id } });
+  // console.log(blog);
+
+  res.render("singleBlog", { blog: blog });
+});
+
+// delete blog page
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  //delete gardey yo blog vanna lai
+  await blogs.destroy({ where: { id: id } });
+  res.redirect("/");
+});
+
 app.post("/createblog", async (req, res) => {
+  // first approach
+  // const title = req.body.title
+  // const description  = req.body.description
+  // const subTitle = req.body.subtitle
+
+  //second approach
   const { title, subtitle, description } = req.body;
   // console.log(title, subtitle, description);
 
@@ -32,7 +61,8 @@ app.post("/createblog", async (req, res) => {
     description: description,
   });
 
-  res.send("form submitted succesfully!");
+  res.redirect("/");
+  // res.send("form submitted succesfully!");
 });
 
 app.listen(3000, function () {
