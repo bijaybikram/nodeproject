0d94404fe3df1b54aba4acc5a628f927190ba7cc
Bijay Bikram <becauseiam33@gmail.com>
2023-09-22 13:17:35 +0545

    starting advance node

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,24 +1,9 @@
 // development
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "",
-//   DB: "sepcms",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
-// production
 module.exports = {
-  HOST: "containers-us-west-136.railway.app",
+  HOST: "localhost",
   USER: "root",
-  PASSWORD: "eDOw7SFJUAOa13F5UDpq",
-  DB: "railway",
+  PASSWORD: "",
+  DB: "sepcms",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -27,3 +12,18 @@ module.exports = {
     idle: 10000,
   },
 };
+
+// production
+// module.exports = {
+//   HOST: "containers-us-west-136.railway.app",
+//   USER: "root",
+//   PASSWORD: "eDOw7SFJUAOa13F5UDpq",
+//   DB: "railway",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
