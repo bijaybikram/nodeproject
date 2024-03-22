29270d84606288197c6a636747abd74017602dac
Bijay Bikram <becauseiam33@gmail.com>
2024-03-22 22:20:41 +0545

    hope it works this time

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,24 +1,9 @@
 // development
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "sepcms",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
-// production
 // module.exports = {
-//   HOST: "o2r.h.filess.io",
-//   USER: "blogspot_shutwonder",
-//   PASSWORD: "4a52e3d5a73054bb5c66d6b30340661651617748",
-//   DB: "blogspot_shutwonder",
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "sepcms",
 //   dialect: "mysql",
 //   pool: {
 //     max: 5,
@@ -27,3 +12,18 @@ module.exports = {
 //     idle: 10000,
 //   },
 // };
+
+// production
+module.exports = {
+  HOST: "o2r.h.filess.io",
+  USER: "blogspot_shutwonder",
+  PASSWORD: "4a52e3d5a73054bb5c66d6b30340661651617748",
+  DB: "blogspot_shutwonder",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
--- services/catchError.js
+++ services/catchError.js
@@ -8,23 +8,23 @@
 //   };
 // };
 
-// module.exports = (fn) => {
-//   return (req, res, next) => {
-//     Promise.resolve(fn(req, res, next)).catch((err) => {
-//       return res.send(err.message || "An error occurred");
-//     });
-//   };
-// };
-
 module.exports = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch((err) => {
-      res.send(err.message);
-      //  const path  =req.route.path
-      //   console.log(err.message)
-      //   req.flash("error","Something went wrong")
-      //   res.redirect(path)
-      //   return
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      return res.send(err.message || "An error occurred");
     });
   };
 };
+
+// module.exports = (fn) => {
+//   return (req, res, next) => {
+//     fn(req, res, next).catch((err) => {
+//       res.send(err.message);
+//       //  const path  =req.route.path
+//       //   console.log(err.message)
+//       //   req.flash("error","Something went wrong")
+//       //   res.redirect(path)
+//       //   return
+//     });
+//   };
+// };
